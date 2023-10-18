b96f5a52e3b0f96ecf459f825e363aa61ad02d43
Bigmikee5 <edehmichael16@gmail.com>
2023-10-18 19:52:49 +0100

    fontsize adjusted at intro2

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,22 +10,9 @@ const Footer = () => {
         <div className="about">
           <h1 className="name">H2018 ALUMINI</h1>
           <p className="description">
-            H2018 Alumni Association is a vibrant union comprising the graduates
-            of the 2020 Biochemistry class from the esteemed Institute of
-            Management and Technology, Enugu. Officially established in 2022,
-            two years subsequent to their academic achievements, the Alumni
-            Association was born from the collective desire to strengthen the
-            camaraderie and foster lasting connections among its members. Beyond
-            its internal objectives, the association holds a profound commitment
-            to making a positive impact within the larger community, actively
-            engaging in various initiatives aimed at social development and
-            progress. The primary aim of the association is to provide a
-            conducive environment for members to establish valuable professional
-            networks, thereby creating opportunities for collaborative growth
-            and development. By nurturing a spirit of unity and support, the
-            association endeavors to serve as a pillar of strength for its
-            members, aiding them in achieving their personal and professional
-            aspirations.
+            The idea of the Alumni Association was conceived when it was
+            considered necessary to create an organization that would forster
+            our unity
           </p>
         </div>
         <div className="contact">
--- src/routes/About.js
+++ src/routes/About.js
@@ -10,7 +10,22 @@ const About = () => {
       <Navbar />
       <Intro2
         heading=" About Us"
-        text="The idea of the Alumni Association was conceived when it was considered necessary to create an organization that would forster our unity"
+        text="H2018 Alumni Association is a vibrant union comprising the graduates
+        of the 2020 Biochemistry class from the esteemed Institute of
+        Management and Technology, Enugu. Officially established in 2022,
+        two years subsequent to their academic achievements, the Alumni
+        Association was born from the collective desire to strengthen the
+        camaraderie and foster lasting connections among its members. Beyond
+        its internal objectives, the association holds a profound commitment
+        to making a positive impact within the larger community, actively
+        engaging in various initiatives aimed at social development and
+        progress. The primary aim of the association is to provide a
+        conducive environment for members to establish valuable professional
+        networks, thereby creating opportunities for collaborative growth
+        and development. By nurturing a spirit of unity and support, the
+        association endeavors to serve as a pillar of strength for its
+        members, aiding them in achieving their personal and professional
+        aspirations."
       />
       <Aboutdetails />
       <Footer />
